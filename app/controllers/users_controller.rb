3a99476ba14b9ac5ae4a00b8dc56eb3caf7e02c1
wissa-azmy <wissa.azmy@gmail.com>
2016-05-21 15:19:13 +0200

    Single user view added (Show Page)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   
   def index
     @users = User.all # getting all the users!
+    # @users = User.paginate(:page => params[:page])
   end
 
   def show
