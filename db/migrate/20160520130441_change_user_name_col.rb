6cf1e453a3d54a30a518c2fcdde3ab0124fcbd5f
Mxheond <mxheond9@gmail.com>
2016-05-22 03:32:47 +0300

    Update group Controller, setting up links for Ajax, Create Partial for group object, Ajaxify the New Group

---
+++ db/migrate/20160520130441_change_user_name_col.rb
@@ -0,0 +1,5 @@
+class ChangeUserNameCol < ActiveRecord::Migration
+  def change
+  	change_column_null :users, :user_name, false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160520091125) do
+ActiveRecord::Schema.define(version: 20160520130441) do
 
   create_table "items", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 20160520091125) do
   create_table "users", force: :cascade do |t|
     t.string   "first_name",             limit: 255
     t.string   "last_name",              limit: 255
-    t.string   "user_name",              limit: 255
+    t.string   "user_name",              limit: 255,              null: false
     t.string   "email",                  limit: 255, default: "", null: false
     t.string   "encrypted_password",     limit: 255, default: "", null: false
     t.string   "reset_password_token",   limit: 255
