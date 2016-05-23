49f029781b28776bc5e51e0a06f72507c2559bec
sherifkhalil <eng_sherifkhalil@hotmail.com>
2016-05-23 18:48:25 +0200

    adding database.yml to gitignor , adding home page with last orders and activity log

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,19 +5,15 @@ class UsersController < ApplicationController
     # @users = User.all # getting all the users!
     # @users = User.paginate(:page => params[:page])
     if user_signed_in? 
-      @id = current_user.id
-     
+
       @user = User.find(current_user.id)
-      @friends = Friendship.where("user_id = ?", current_user.id)
+      @friends = Friend.where("user_id = ?", current_user.id)
       @user_friends = []
       @friends.each do |friend| 
         @id = friend.friend_id
-        puts @id
         @user_friends << User.find_by(id: @id)
-      end
-      @orders = Order.last(3)
-      # # @orders = Order.all() 
-      # @orders.user_id = @id 
+    end
+      @orders = Order.last(4)
     else
       redirect_to new_user_session_url
     end
